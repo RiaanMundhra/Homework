9ef429e0bb0d3c1bf3535e6aea9242016ad3ccb1
RiaanMundhra <165542789+RiaanMundhra@users.noreply.github.com>
2025-03-27 10:52:03 +0530

    Add files via upload

---
+++ pos&neg.py
@@ -0,0 +1,7 @@
+num = int(input("write any number:"))
+if num > 0:
+    print("your number is positive")
+elif num == 0:
+    print("your number is not positive or negetive")
+else:
+    print("your number is negetive")
