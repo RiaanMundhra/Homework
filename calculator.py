f7ab22d157ae0610bf806e166706ba7441d4bbae
RiaanMundhra <165542789+RiaanMundhra@users.noreply.github.com>
2025-05-29 11:46:15 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,18 @@
+a = 100
+b = 10
+
+def add(a,b):
+    c = a + b
+    print(c)
+
+def sub(a,b):
+    d = a - b
+    print(d)
+
+def mul(a,b):
+    e = a * b
+    print(e)
+
+def div(a,b):
+    f = a / b
+    print(f)
